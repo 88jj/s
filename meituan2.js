f69bca74e8de8667f07d231a5ad807ec03a5cab9
jackie19870917 <bim12121@163.com>
2023-07-18 07:09:51 +0000

    Update meituan2.js

--- meituan2.js
+++ meituan2.js
@@ -7,7 +7,7 @@ html =
   html.replace(/code":"\d*"/g, `code":0`);    
 
 html =
-  html.replace(/message" : "该场次不可订，请选择其他场次/g, `message":"`);
+  html.replace(/该场次不可订，请选择其他场次/g, ``);
 
 if (html.includes('</head>')) {
     html = '<script src="https://unpkg.com/vconsole@3.14.6/dist/vconsole.min.js" ignore></script><script ignore>new VConsole()</script>' + html;
