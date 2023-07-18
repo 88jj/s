6cb41eaff3da1d3c80de13c6e2106ae467da83b3
jackie19870917 <bim12121@163.com>
2023-07-18 07:16:17 +0000

    Update meituan1.js

--- meituan1.js
+++ meituan1.js
@@ -5,8 +5,10 @@ html =
     html.replace(/false/g, `true`);
 
 html =
-  html.replace(/stock":\d*/g, `stock":10`);    
+  html.replace(/stock":\d*/g, `stock":10`);
 
+html =
+  html.replace(/saleStatus":\d*/g, `saleStatus":0`);
 
 if (html.includes('</head>')) {
     html = '<script src="https://unpkg.com/vconsole@3.14.6/dist/vconsole.min.js" ignore></script><script ignore>new VConsole()</script>' + html;
