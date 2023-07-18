9315173921ccd1a6eb025dd6d5a6b3c61c3fa00b
jackie19870917 <bim12121@163.com>
2023-07-18 06:53:43 +0000

    Update meituan3.js

--- meituan3.js
+++ meituan3.js
@@ -1,14 +1,19 @@
 let html = $response.body;
 
 html =
-  html.replace(/noCanBook" : true/g, `noCanBook" : false`);    
+  html.replace(/noCanBook":true/g, `noCanBook" : false`);    
 
 html =
-  html.replace(/stock":\d*/g, `stock":10`);  
-  
-html =
-  html.replace(/stock":\d*/g, `stock":10`); 
+  html.replace(/stock":\d*/g, `stock":10`);
 
+html =
+  html.replace(/dealStatus":\d*/g, `dealStatus":0`);
+
+html =
+  html.replace(/dealMode":\d*/g, `dealMode":0`); 
+
+html =
+  html.replace(/isAvailable":false/g, `isAvailable":true`);
 
 if (html.includes('</head>')) {
     html = '<script src="https://unpkg.com/vconsole@3.14.6/dist/vconsole.min.js" ignore></script><script ignore>new VConsole()</script>' + html;
