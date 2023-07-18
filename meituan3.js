59ea78f7ad9d244f25edf0c97190036f97b15693
jackie19870917 <bim12121@163.com>
2023-07-18 08:33:55 +0000

    Update meituan3.js

--- meituan3.js
+++ meituan3.js
@@ -1,21 +1,15 @@
 let html = $response.body;
 
-html =
-  html.replace(/stock":\d*/g, `stock":10`);
+html = html.replace(/stock":\d*/g, `stock":10`);
 
-html =
-  html.replace(/dealStatus":\d*/g, `dealStatus":0`);
+html = html.replace(/status":\d*/g, `status":1`);
 
-html =
-  html.replace(/dealMode":\d*/g, `dealMode":0`); 
+html = html.replace(/dealStatus":\d*/g, `dealStatus":0`);
 
-html =
-  html.replace(/isAvailable":false/g, `isAvailable":true`);
+html = html.replace(/isAvailable":false/g, `isAvailable":true`);
 
 if (html.includes('</head>')) {
     html = '<script src="https://unpkg.com/vconsole@3.14.6/dist/vconsole.min.js" ignore></script><script ignore>new VConsole()</script>' + html;
 }
-
-
 $done({ body: html});
 
