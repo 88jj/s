8008c3e4c918fe368c73de5ebcb5ab8206df6f40
jackie19870917 <bim12121@163.com>
2023-07-18 09:50:09 +0000

    Update meituan3.js

--- meituan3.js
+++ meituan3.js
@@ -1,7 +1,7 @@
 let html = $response.body;
 
 html = html.replace(/stock":\d*/g, `stock":10`);
-
+html = html.replace(/noCanBook":true/g, `noCanBook":false`);
 html = html.replace(/status":\d*/g, `status":1`);
 html = html.replace(/isAvailable":false/g, `isAvailable":true`);
 if (html.includes('</head>')) {
