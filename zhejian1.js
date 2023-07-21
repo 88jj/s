ae80ac7580ad6d1649db7d40c50893d3c1aac901
jackie19870917 <bim12121@163.com>
2023-07-21 18:36:08 +0800

    Update zhejian1.js

--- zhejian1.js
+++ zhejian1.js
@@ -2,7 +2,7 @@ let html = $response.body;
 
 html = html.replace(/21005/g, `0`);
 
-html = html.replace(/stock":"0/g,`stock":"33`);
+html = html.replace(/stock":"0"/g,`stock":"33"`);
 
 html = html.replace(/status":\d*/g,`status":1`);
 
