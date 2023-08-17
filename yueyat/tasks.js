c9967cab126868a171d38f8dc3027f146c0e88b9
jackie19870917 <bim12121@163.com>
2023-08-17 14:03:51 +0800

    Create tasks.js

---
+++ yueyat/tasks.js
@@ -0,0 +1,10 @@
+{
+    "name":"锡林劵",
+    "task":[
+        {
+            "config":"59 59 9 * * * https:\/\/raw.githubusercontent.com\/crossutility\/Quantumult-X\/master\/sample-task.js, img-url=https:\/\/raw.githubusercontent.com\/crossutility\/Quantumult-X\/master\/quantumult-x.png, tag=2023-08, enabled=true",
+            "addons":"https:\/\/raw.githubusercontent.com\/crossutility\/Quantumult-X\/master\/sample-import-rewrite.txt"
+        }
+    ],
+    "description":"锡林劵"
+}
