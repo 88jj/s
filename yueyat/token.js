58b3240c90d7825de86719e6485be7154a2b8bd7
jackie19870917 <bim12121@163.com>
2023-08-17 16:19:19 +0800

    Update token.js

--- yueyat/token.js
+++ yueyat/token.js
@@ -4,9 +4,23 @@ $prefs.setValueForKey(value, key)
 读取本地库的值
 $prefs.valueForKey(key)
 **/
-
 let key = `yueyat_tokens`;
-
 let tokens = $prefs.valueForKey(key);
-
 console.log(`本地已存在的token = ${tokens}`);
+// 是否清除token
+let clearToken = 1;
+if (clearToken){
+  $prefs.setValueForKey('', key);
+}
+// 这里设置新的token
+let value = '';
+
+if (value){
+  if(tokens){
+    value = tokens + ',' + value;
+  }
+  console.log(`修改新值key=${key} value=${value}`);
+  $prefs.setValueForKey(value, key);
+}
+
+$done();
