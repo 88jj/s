c9fd7ff7a3213eaf82fc976e35db6b42c7d134c9
jackie19870917 <bim12121@163.com>
2023-08-17 14:50:59 +0800

    Update tasks.js

--- yueyat/tasks.js
+++ yueyat/tasks.js
@@ -4,6 +4,10 @@
         {
             "config":"59 59 9 * * * https:\/\/88jj.github.io\/s\/yueyat\/task154.js, img-url=https:\/\/raw.githubusercontent.com\/crossutility\/Quantumult-X\/master\/quantumult-x.png, tag=100劵, enabled=true",
             "addons":""
+        },
+        {
+            "config":"59 59 9 * * * https:\/\/88jj.github.io\/s\/yueyat\/task153.js, img-url=https:\/\/raw.githubusercontent.com\/crossutility\/Quantumult-X\/master\/quantumult-x.png, tag=50劵, enabled=true",
+            "addons":""
         }
     ],
     "description":"锡林劵"
