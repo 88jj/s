afea8425d6566d0c28dd3ea823d4b35a4470cb2f
jackie19870917 <bim12121@163.com>
2023-08-17 17:01:43 +0800

    Update token.js

--- yueyat/token.js
+++ yueyat/token.js
@@ -7,14 +7,23 @@ $prefs.valueForKey(key)
 let key = `yueyat_tokens`;
 let tokens = $prefs.valueForKey(key);
 console.log(`本地已存在的token = ${tokens}`);
+
+// 获取抓包的token
+let newToken = $request.headers['Authorization'];
+
 // 是否清除token, 1清除旧的token，0不清除旧的token
 let clearToken = 1;
 if (clearToken){
   tokens = '';
   $prefs.setValueForKey(tokens, key);
+  console.log(`清空本地value，key=${key}`);
 }
 // 这里设置新的token
 let value = '';
+
+if (newToken){
+  value = newToken;
+}
 
 if (value){
   if(tokens){
