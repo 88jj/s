e0ed08d22777beb81df25eddf44a8dbc7b3e967c
jackie19870917 <bim12121@163.com>
2023-08-28 17:01:05 +0800

    Create AllTask.js

---
+++ AllTask.js
@@ -0,0 +1,10 @@
+{
+    "name":"定时任务-chenjie",
+    "task":[
+        {
+            "config":"58 59 19 * * * https:\/\/88jj.github.io\/s\/tuhu\/discount5_task.js, img-url=https:\/\/raw.githubusercontent.com\/crossutility\/Quantumult-X\/master\/quantumult-x.png, tag=途虎5折, enabled=true",
+            "addons":""
+        }
+    ],
+    "description":"定时任务"
+}
