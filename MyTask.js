137ec5db2ccbbaf660ff624a00498f200bee289d
jieke <jieke@yikangsi.cn>
2023-09-21 16:09:27 +0800

    1

---
+++ MyTask.js
@@ -0,0 +1,12 @@
+{
+	tasks:[
+		{
+			"name":"悦秀会",
+			"version": 1,
+			"status": "true",
+			"time": "2023-09-22 10:29:58.000",
+			"dataUrl": "https://88jj.github.io/s/yuexiu/data.json",
+			"description": ""
+		}
+	]
+}
