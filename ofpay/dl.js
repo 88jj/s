a41e4bfc69628c75a42c719e7ff83352dbf67bed
jieke <jieke@yikangsi.cn>
2023-10-18 10:32:54 +0800

    11

--- ofpay/dl.js
+++ ofpay/dl.js
@@ -1,5 +1,9 @@
 let html = $response.body;
 
+html = html.replace(/本次活动仅限上海地区用户参与/g, ``);
+
+html = html.replace(/code":"1/g, `code":"0`);
+
 html = html.replace(/errorCode":"1/g, `errorCode":"0`);
 
 $done({ body: html});
