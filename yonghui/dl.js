00a473b1bfdae7777bd365911ca03ca0e4b348ce
jieke <jieke@yikangsi.cn>
2023-10-31 09:23:55 +0800

    cj

--- yonghui/dl.js
+++ yonghui/dl.js
@@ -1,5 +1,6 @@
 let html = $response.body;
 
 html = html.replace(/"couponreceivestate":"WAIT_RECEIVED"/g, `"totalStock":"CAN_RECEIVE"`);
+html = html.replace(/"starttime":1698721860000/g, `"starttime":1698715299000`);
 
 $done({ body: html});
